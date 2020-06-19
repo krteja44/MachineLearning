4e59a970364f3cccc4b22980e0cd9e48abd68432
krteja44 <47230256+krteja44@users.noreply.github.com>
2020-06-19 01:41:05 -0400

    completed svm algorithm practice

--- course1/projects/svm/svm_author_id.py
+++ course1/projects/svm/svm_author_id.py
@@ -12,6 +12,9 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import numpy as np
 
 
 ### features_train and features_test are the features for the training
@@ -24,7 +27,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+clf = SVC(kernel= 'rbf', C = 10000.0)
+features_train = features_train[:len(features_train)//100]
+labels_train = labels_train[:len(labels_train)//100]
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time", round(time()-t0, 3), 's')
+t0 = time()
+pred = clf.predict(features_test)
+#print(pred[10], pred[26], pred[50])
+#print(len(pred[pred==1]))
+print(len(pred[np.where(pred==1)]))
+print("predicting time", round(time()-t0, 3), 's')
+print(accuracy_score(labels_test, pred))
 #########################################################
 
 
