28ac7ddb5892719e87f89cf2e9ffae1d02731403
krteja44 <47230256+krteja44@users.noreply.github.com>
2020-07-08 18:35:48 -0400

    added K Means code

--- Udacity_Intro_to_ML/mini_projects/k_means/k_means_cluster.py
+++ Udacity_Intro_to_ML/mini_projects/k_means/k_means_cluster.py
@@ -39,7 +39,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
@@ -48,29 +48,64 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
-### for f1, f2, _ in finance_features:
+### for f1, f2, f3 in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+for f1, f2, f3 in finance_features:
+    plt.scatter( f2, f3 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+from sklearn.cluster import KMeans
 
+clf = KMeans(n_clusters=3)
 
-
+pred = clf.fit_predict(finance_features)
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+    print ("no predictions object named pred found, no clusters to plot")
+    
+    
+# In the next lesson, we’ll talk about feature scaling. 
+# It’s a type of feature preprocessing that you should perform before 
+# some classification and regression tasks. Here’s a sneak preview that 
+# should call your attention to the general outline of what feature scaling does.
+
+# What are the maximum and minimum values taken by the “exercised_stock_options” 
+# feature used in this example?
+
+# (NB: if you look at finance_features, there are some "NaN" values that have been 
+# cleaned away and replaced with zeroes--so while those might look like the minima, 
+# it's a bit deceptive because they're more like points for which we don't have information, 
+# and just have to put in a number. So for this question, go back to data_dict 
+# and look for the maximum and minimum numbers that show up there, 
+# ignoring all the "NaN" entries.)
+    
+    
+stock = []
+salary = []
+for key in data_dict:
+    if (data_dict[key]["exercised_stock_options"] ==  'NaN'):
+        pass
+    else:
+        stock.append(float(data_dict[key]["exercised_stock_options"]))
+    if (data_dict[key]["salary"] ==  'NaN'):
+        pass
+    else:
+        salary.append(float(data_dict[key]["salary"]))
+        
+print('Excersied stock options max: ', max(stock),' min: ', min(stock))
+print('Salary max: ', max(salary),' min: ', min(salary))
